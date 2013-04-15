aa9b7283ab6ed8e52120c0ca554777a40ba8c1d6
Alexandra <alexandra.banasova@return2sender.ie>
2013-04-15 12:30:22 +0100

    loading files

---
+++ src/com/example/vodafonewelcome/Welcome.java
@@ -0,0 +1,23 @@
+package com.example.vodafonewelcome;
+
+import android.os.Bundle;
+import android.view.Menu;
+import org.apache.cordova.DroidGap;
+
+public class Welcome extends DroidGap {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.welcome, menu);
+        return true;
+    }
+    
+}
